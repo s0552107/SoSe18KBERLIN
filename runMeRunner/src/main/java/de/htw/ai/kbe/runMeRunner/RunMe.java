a16c7a2bb9d5a96fbb15ef41b2df08b7d7b296b2
s0552107 <s0552107@htw-berlin.de>
2018-04-25 13:40:29 +0200

    stand 25.04

---
+++ runMeRunner/src/main/java/de/htw/ai/kbe/runMeRunner/RunMe.java
@@ -0,0 +1,14 @@
+package de.htw.ai.kbe.runMeRunner;
+
+import java.lang.annotation.*;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+	
+public @interface RunMe {
+	  String input();
+}
+	
+	
+	
